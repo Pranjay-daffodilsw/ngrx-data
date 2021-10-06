f5cf6abf92c7e9ac2a2228915d5534de9fa34669
Pranjay <pranjay@daffodilsw.com>
2021-10-06 15:35:07 +0530

    new application setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { EntityDataModule } from '@ngrx/data';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { HomeComponent } from './home/home.component';
+import { entityConfig } from './entity-metadata';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsListComponent,
+    SinglePostComponent,
+    EditPostComponent,
+    AddPostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    EntityDataModule.forRoot(entityConfig),
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
